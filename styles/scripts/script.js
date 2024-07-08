2d26440207e21b1e301037f77fcac7ecb49bc8c0
shiono-gif <shizu.shiono@gmail.com>
2024-07-08 15:49:05 +0900

    Add files via upload

---
+++ styles/scripts/script.js
@@ -0,0 +1,57 @@
+'use script';
+
+        /* -------　カルーセル　------- */
+        $(function () {
+            $(".slider").slick({
+                arrows: false,
+                autoplay: true,//自動再生
+                autoplaySpeed: 2000,
+                speed: 1000,
+                slidesToShow: 3,//画面に画像を何枚みせるか
+                slidesToScroll: 3,//1回のスクロールで移動させる枚数
+                centerPadding: "10%",//前後の画像をどのくらい見せるか
+                adaptiveHeight: true,
+                centerMode: true,//左右に画像を表示させる
+                dots: true,//ドットを表示させる
+                arrows: true, // デフォルトの矢印を表示させる
+                appendArrows: $('.arrow_box'),//指定の要素に矢印を入れる
+                prevArrow: '<div class="slide-arrow prev-arrow"></div>',
+                nextArrow: '<div class="slide-arrow next-arrow"></div>',
+                responsive: [
+                    {
+                    breakpoint: 768,//横幅が768px以下の見せ方
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        centerPadding: "30px",
+                    }
+                    }
+                ]
+            });
+        });
+
+/* -------　nav　------- */
+$("#openNav").click(function () {//ボタンがクリックされたら
+    $("#nav").toggleClass('show');//showクラスを付与
+  });
+  
+  $("#closeNav").click(function () {//ボタンがクリックされたら
+      $("#nav").removeClass('show');//showクラスを除去
+  });
+
+  /*----- top bottan---*/
+
+$(document).ready(function() {
+    var pagetop = $('.pagetop');
+      $(window).scroll(function () {
+         if ($(this).scrollTop() > 100) {
+              pagetop.fadeIn();
+         } else {
+              pagetop.fadeOut();
+              }
+         });
+         pagetop.click(function () {
+             $('body, html').animate({ scrollTop: 0 }, 500);
+                return false;
+     });
+  });
